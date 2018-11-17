920c7001d16a59f6db29a461e77c9f119776702c
Washington <mvsude2018@outlook.es>
2018-11-16 22:50:08 -0300

    Extendi clase Carrito

---
+++ Core/LogicaPersistencia/Modelo Extendido/Carrito.cs
@@ -0,0 +1,34 @@
+﻿using Modelo.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaPersistencia
+{
+    public partial class Carrito
+    {
+        /*public Carrito()
+        {
+
+        }*/
+
+        public Carrito(CarritoVO vo)
+        {
+            this.CarritoId = vo.IdCarrito;
+            this.UsuarioId = vo.IdCliente;
+            this.MonedaId = vo.IdMoneda;
+            this.CarritoFechaCreacion = vo.FechaCreacion;
+        }
+
+        public CarritoVO DarCarrito()
+        {
+
+            return new CarritoVO(CarritoId, UsuarioId, CarritoFechaCreacion, MonedaId);
+
+        }
+
+
+    }
+}
