70c3ce77802b83567bd894aee207c7e5f45d9585
Maximiliano Arias <maxim_arias_iliano@yahoo.com.ar>
2018-11-18 17:47:03 -0300

    Se agrega webreference de prueba en proyecto ConsolaPrueba

--- Core/ConsolaPrueba/Program.cs
+++ Core/ConsolaPrueba/Program.cs
@@ -1,10 +1,11 @@
-﻿using LogicaPersistencia.DAO;
-using Modelo.ValueObjects;
+﻿//using LogicaPersistencia.DAO;
+//using Modelo.ValueObjects;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ConsolaPrueba.TiendaVirtualWebservice;
 
 namespace ConsolaPrueba
 {
@@ -12,9 +13,18 @@ namespace ConsolaPrueba
     {
         static void Main(string[] args)
         {
-            CategoriaVO catvo = new CategoriaVO(1, "Computadoras", "Computadoras y notebooks", true);
-            CategoriaDAO catdao = new CategoriaDAO();
-            catdao.InsertarCategoria(catvo);
+            //CategoriaVO catvo = new CategoriaVO(1, "Computadoras", "Computadoras y notebooks", true);
+            //CategoriaDAO catdao = new CategoriaDAO();
+            //catdao.InsertarCategoria(catvo);
+
+            WebServiceSoapClient ws = new WebServiceSoapClient();
+            CategoriaVO catvo = new CategoriaVO()
+            {
+                Nombre = "Computadoras",
+                Descripcion = "Computadoras y notebooks",
+                Habilitado = true
+            };
+            ws.InsertarCategoria(new CategoriaVO());
         }
     }
 }
--- Core/Modelo/ValueObjects/CategoriaVO.cs
+++ Core/Modelo/ValueObjects/CategoriaVO.cs
@@ -5,26 +5,30 @@ namespace Modelo.ValueObjects
     public class CategoriaVO
     {
         // Atributos
-        private int idCategoria;
-        private String nombre;
-        private String descripcion;
-        private Boolean habilitado;
+        //private int idCategoria;
+        //private String nombre;
+        //private String descripcion;
+        //private Boolean habilitado;
 
         // Properties
-        public int IdCategoria { get { return idCategoria; } }
-        public string Nombre { get { return nombre; } }
-        public string Descripcion { get { return descripcion; } }
-        public Boolean Habilitado { get { return habilitado; } }
+        public int IdCategoria { get; set; }
+        public string Nombre { get; set; } 
+        public string Descripcion { get; set; }
+        public Boolean Habilitado { get; set; }
+        //public int IdCategoria { get { return idCategoria; } }
+        //public string Nombre { get { return nombre; } }
+        //public string Descripcion { get { return descripcion; } }
+        //public Boolean Habilitado { get { return habilitado; } }
 
         // Constructores
         public CategoriaVO() { }
 
         public CategoriaVO(int idc, string nom, string desc, Boolean hab)
         {
-            idCategoria = idc;
-            nombre = nom;
-            descripcion = desc;
-            habilitado = hab;
+            IdCategoria = idc;
+            Nombre = nom;
+            Descripcion = desc;
+            Habilitado = hab;
         }
     }
 }
