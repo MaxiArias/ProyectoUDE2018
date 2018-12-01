a9731aef66839896a29cdc6bb3e41e1d852a9692
Washington <mvsude2018@outlook.es>
2018-12-01 15:43:50 -0300

    Agrego webapi

---
+++ Core/WebApi/Models/UsuarioController.cs
@@ -0,0 +1,21 @@
+﻿using LogicaPersistencia;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApi.Models
+{
+    public class UsuarioController : ApiController
+    {
+
+        public bool GetUsuarioLogin(String mail, String password)
+        {
+            IFachadaWeb fac = new FabricaFachadas().CrearFachadaWeb;
+            return fac.UsuarioLogin(mail, password);
+        }
+    }
+
+}
