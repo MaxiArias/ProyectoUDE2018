9b05665d9b2ea18f66429b3c3361fef48d4c394c
Maximiliano Arias <maxim_arias_iliano@yahoo.com.ar>
2018-12-02 13:40:17 -0300

    Agrego tipoError y excepciones

--- Core/LogicaPersistencia/Enumerados.cs
+++ Core/LogicaPersistencia/Enumerados.cs
@@ -19,5 +19,27 @@
             Persona = 0,
             Empresa = 1
         }
+
+        public enum TipoError
+        {
+            Ok = 0,
+            MailYaExiste = 1,
+            MailNoExiste = 2,
+            CategoriaYaExiste = 3,
+            CategoriaNoExiste = 4,
+            BackofficeNoExiste = 5,
+            MonedaYaExiste = 6,
+            MonedaNoExiste = 7,
+            ProductoYaExiste = 8,
+            ProductoNoExiste = 9,
+            StockInsuficiente = 10,
+            RolYaExiste = 11,
+            RolNoExiste = 12,
+            CedulaYaExiste = 13,
+            ClientYaExiste = 14,
+            ClienteNoExiste = 15,
+            CarritoYaExiste = 16,
+            CarritoNoExiste = 17
+        }
     }
 }
